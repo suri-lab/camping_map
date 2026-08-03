3836ffe4810ce667131ddcc7bc9e59d807019e8b
수리랩 <80581450+suri-lab@users.noreply.github.com>
2026-08-03 10:06:24 +0900

    서비스키를 환경 변수로 분리
    코드에 평문으로 노출되지 않도록 CAMPING_SERVICE_KEY 환경 변수 사용

--- api/camping.js
+++ api/camping.js
@@ -1,7 +1,5 @@
-const SERVICE_KEY = 'MKEVIcI87d%2FV%2FeMK06XvZnD2lGJ3qh8B9JqI%2FjyiNZjPcOzZO%2BXxIRHA9qoogQ6lUhgxCzYv5L%2FUpVHyTGZeTQ%3D%3D';
-
 module.exports = async function handler(req, res) {
-    const url = `https://apis.data.go.kr/B551011/GoCamping/basedList?serviceKey=${SERVICE_KEY}&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=TestApp&_type=json`;
+    const url = `https://apis.data.go.kr/B551011/GoCamping/basedList?serviceKey=${process.env.CAMPING_SERVICE_KEY}&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=TestApp&_type=json`;
 
     try {
         const response = await fetch(url);
